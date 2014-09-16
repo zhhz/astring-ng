45bc3e6e935b50b05cefefe3a26f7362680d79b7
Zhonghai Zuo <zhonghai.zuo@haygroup.com>
2014-09-16 16:15:15 -0400

    chore: adds gulp-ga and gulp-gsub plugins

--- gulpfile.js
+++ gulpfile.js
@@ -9,6 +9,8 @@ var gulp = require('gulp'),
   git = require('gulp-git'),
   bump = require('gulp-bump'),
   tagVersion = require('gulp-tag-version'),
+  ga = require('gulp-ga'),
+  gsub = require('gulp-gsub'),
   bowerFiles = require('main-bower-files');
 
 var vendorFiles = bowerFiles();
@@ -74,37 +76,6 @@ gulp.task('watch', ['js', 'lib','css', 'fonts', 'songs'], function() {
   gulp.watch('test/**/*.js').on('change', livereload.changed);
 });
 
-gulp.task('dist', ['css', 'fonts', 'songs'], function(){
-  // compile app js
-  gulp.src(['app/public/js/app.js', 'app/public/js/**/*.js', '!app/public/js/**/*.min.js'])
-    .pipe(concat('app.min.js'))
-    .pipe(ngAnnotate())
-    .pipe(uglify())
-    .pipe(gulp.dest('dist/public/js'));
-
-  // compile bower components - js
-  var jsFilter = filter('*.js');
-  gulp.src(vendorFiles)
-    .pipe(jsFilter)
-      .pipe(concat('lib.min.js'))
-      .pipe(uglify())
-      .pipe(gulp.dest('dist/public/js'))
-    .pipe(jsFilter.restore());
-
-  // all the other public files/folders
-  gulp.src(['app/public/**', '!app/public/js/**'])
-    .pipe(gulp.dest('dist/public/'));
-
-  // server files
-  gulp.src(['app/server/**', '!app/server/data/songs/**'])
-    .pipe(gulp.dest('dist/server'))
-
-  // misc
-  gulp.src(['LICENSE-MIT', 'Procfile'])
-    .pipe(gulp.dest('dist/'))
-
-});
-
 /**
  * Bumping version number and tagging the repository with it.
  * Please read http://semver.org/
@@ -131,5 +102,44 @@ function inc(importance) {
 gulp.task('patch', function() { return inc('patch'); });
 gulp.task('minor', function() { return inc('minor'); });
 gulp.task('major', function() { return inc('major'); });
+
+gulp.task('release', ['css', 'fonts', 'songs'], function(){
+  var pkg = require('./package.json');
+  var version = pkg.version;
+
+  // compile app js
+  gulp.src(['app/public/js/app.js', 'app/public/js/**/*.js', '!app/public/js/**/*.min.js'])
+    .pipe(concat('app.min.' + version + '.js'))
+    .pipe(ngAnnotate())
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/public/js'));
+
+  // compile bower components - js
+  var jsFilter = filter('*.js');
+  gulp.src(vendorFiles)
+    .pipe(jsFilter)
+      .pipe(concat('lib.min.' + version + '.js'))
+      .pipe(uglify())
+      .pipe(gulp.dest('dist/public/js'))
+    .pipe(jsFilter.restore());
+
+  // all the other public files/folders
+  gulp.src(['app/public/**', '!app/public/js/**', '!app/public/index.html'])
+    .pipe(gulp.dest('dist/public/'));
+
+  // server files
+  gulp.src(['app/server/**', '!app/server/data/songs/**'])
+    .pipe(gulp.dest('dist/server'))
+
+  // misc
+  gulp.src(['LICENSE-MIT', 'Procfile'])
+    .pipe(gulp.dest('dist/'))
+
+  // add google analytics, gsub with the compiled js
+  gulp.src(['app/public/index.html'])
+    .pipe(ga({url: 'a-string.us', uid: 'UA-44119310-1'}))
+    .pipe(gsub({source: 'min.js', target:  'min.' + version + '.js'}))
+    .pipe(gulp.dest('dist/public/'));
+});
 
 gulp.task('default', ['js', 'lib', 'css', 'fonts', 'songs']);
