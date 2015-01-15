9862c446ea90011adb5b8a3a24f2d44614dd2857
Zhonghai Zuo <zhonghai.zuo@gmail.com>
2015-01-15 17:26:23 -0500

    fix: fixed the link on navbar brand, it will take user to today's todo list

--- app/public/js/controllers/todo.ctrl.js
+++ app/public/js/controllers/todo.ctrl.js
@@ -1,7 +1,15 @@
 angular.module('a-string')
-.controller('TodoCtrl', ['States',
-  function TodoCtrl(States) {
+.controller('TodoCtrl', ['States', '$location',
+  function TodoCtrl(States, $location) {
     States.init();
-    this.states = States;
+
+    var self = this;
+    self.states = States;
+
+    self.gotoTodos = function(){
+      States.activeMenu = 'todos';
+      States.setDate(moment().format('YYYY-MM-DD'));
+      $location.path('/todos');
+    };
   }
 ]);
