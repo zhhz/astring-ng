30250f8423f17581278d09486f75747c2bb355d0
Zhonghai Zuo <zhonghai.zuo@gmail.com>
2015-07-06 18:52:22 -0400

    the token's expiration days is 365 - a year

--- app/server/ctrls/auth.ctrl.js
+++ app/server/ctrls/auth.ctrl.js
@@ -12,7 +12,7 @@ function createToken(user) {
   var payload = {
     sub: user._id,
     iat: moment().unix(),
-    exp: moment().add(14, 'days').unix(),
+    exp: moment().add(365, 'days').unix(),
     dis: user.displayName
   };
   return jwt.encode(payload, config.TOKEN_SECRET);
